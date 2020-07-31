be382497b150339990f673ad86e9df5ffbb16671
emerald <effallarme@gmail.com>
2020-07-31 19:52:10 +0800

    Keeping Aves inherited for Rooster to avoid other unnecessary inheritance

---
+++ src/main/java/animals/Rooster.java
@@ -0,0 +1,18 @@
+package animals;
+
+import action.NoisyAnimal;
+
+public class Rooster extends Aves implements NoisyAnimal {
+    private String noise = "Cock-a-doodle-doo";
+
+    @Override
+    public String say() {
+        System.out.println(noise);
+        return noise;
+    }
+
+    @Override
+    public String getNoise() {
+        return null;
+    }
+}
